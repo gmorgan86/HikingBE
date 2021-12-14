24fc5116abc3bf258eff399b5a6893e019c4846d
gareth.morgan <garethmorgan86@gmail.com>
2021-12-14 16:11:00 +0000

    made initial files

---
+++ src/main/java/com/qa/Hiking1/SpringHiking1Application.java
@@ -0,0 +1,13 @@
+package com.qa.Hiking1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHiking1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHiking1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/qa/Hiking1/SpringHiking1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.Hiking1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHiking1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

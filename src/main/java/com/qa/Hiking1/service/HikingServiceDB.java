344a84b66d2a701c1c2f9b974f7965cc8c90a93b
gareth.morgan <garethmorgan86@gmail.com>
2021-12-14 16:25:33 +0000

    SFP-25 #Commment added serviceDB class and Repo

--- src/main/java/com/qa/Hiking1/service/HikingServiceDB.java
+++ src/main/java/com/qa/Hiking1/service/HikingServiceDB.java
@@ -1,5 +1,82 @@
 package com.qa.Hiking1.service;
 
-public class HikingServiceDB {
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.qa.Hiking1.domain.Hiking;
+import com.qa.Hiking1.repo.HikingRepo;
+
+
+
+@Service
+public class HikingServiceDB implements HikingService {
+
+	private HikingRepo repo;
+
+	@Autowired
+	public HikingServiceDB(HikingRepo repo) {
+		super();
+		this.repo = repo;
+	}
+
+	@Override
+	public Hiking createHill(Hiking hills) {
+		Hiking created = this.repo.save(hills);
+		return created;
+	}
+
+	@Override
+	public List<Hiking> getAllHills() {
+		return this.repo.findAll();
+	}
+
+	@Override
+	public Hiking getHill(Integer id) {
+		Optional<Hiking> found = this.repo.findById(id);
+		return found.get();
+	}
+
+
+	
+	@Override
+	public List<Hiking> getByMountainName(String mountainName) {
+		return this.repo.findByMountainNameIgnoreCase(mountainName);
+	}
+
+	@Override
+	public List<Hiking> getByRegion(String region) {
+		return this.repo.findByRegionIgnoreCase(region);
+	}
+
+	@Override
+	public List<Hiking> getByCountry(String country) {
+		return this.repo.findByCountryIgnoreCase(country);
+	}
+
+	@Override
+	public Hiking replaceHill(Integer id, Hiking newHill) {
+		Hiking existing = this.repo.findById(id).get();
+
+		existing.setId(newHill.getId());
+		existing.setMountainName(newHill.getMountainName());
+		existing.setRegion(newHill.getRegion());
+		existing.setHeight(newHill.getHeight());
+		existing.setCountry(newHill.getCountry());
+
+		Hiking updated = this.repo.save(existing);
+
+		return updated;
+	}
+
+	@Override
+	public void deleteHill(Integer id) {
+		this.repo.deleteById(id);
+
+	}
+
+	
 
 }
